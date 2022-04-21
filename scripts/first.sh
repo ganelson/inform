bb1bb75578d2ee405b39e3302c302f00d9afa0f8
Graham Nelson <graham.nelson@mod-langs.ox.ac.uk>
2022-04-22 00:09:24 +0100

    Migrated to new makescript format

--- scripts/first.sh
+++ scripts/first.sh
@@ -1,6 +1,6 @@
 echo "(A script to make a first build of the core Inform tools)"
 echo "(Step 1 of 4: making the overall makefile)"
-if ! ( ../inweb/Tangled/inweb -prototype scripts/makescript.txt -makefile makefile; ) then
+if ! ( ../inweb/Tangled/inweb -prototype scripts/inform.mkscript -makefile makefile; ) then
 	echo "(Okay, so that failed. Have you installed and built Inweb?)"
 	exit 1
 fi
